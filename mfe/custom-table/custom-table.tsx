03e59ea9ba8e5c413990296b8cafcfb2c4532555
abhishekra07 <abhira719@gmail.com>
2021-09-24 12:22:47 +0530

    Ag-Grid Initialization

--- mfe/custom-table/custom-table.tsx
+++ mfe/custom-table/custom-table.tsx
@@ -1,18 +1,33 @@
 import {html, css, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
+import { Grid, GridOptions }  from 'ag-grid-community';
+
+import './custom-table.css';
+
 @customElement("custom-table")
 export class CustomTable extends LitElement {
-  static styles = css`
-    p {
-      color: blue;
-    }
-  `;
 
-  @property()
-  name = "Somebody";
+  gridOptions: GridOptions = {};
+
+  firstUpdate(){
+    this.gridOptions = {
+      columnDefs: [
+          { field: "make" },
+          { field: "model" },
+          { field: "price" }
+        ],
+      rowData: [
+          { make: "Toyota", model: "Celica", price: 35000 },
+          { make: "Ford", model: "Mondeo", price: 32000 },
+          { make: "Porsche", model: "Boxter", price: 72000 }
+        ]
+    }
+    let eGridDiv: HTMLElement = document.getElementById('myGrid');
+    new Grid(eGridDiv, this.gridOptions);
+  }
 
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`<div id="myGrid" style="height: 200px; width:500px;" class="ag-theme-material"></div>`;
   }
 }
